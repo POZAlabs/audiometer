7c6e8052a171a0e84a8b082d28197a824a00a754
SunWoong KIM <35526032+novdov@users.noreply.github.com>
2022-09-01 22:09:45 +0900

    IP-657: [audiometer] 빌드 오류 수정 (#3)

--- build.py
+++ build.py
@@ -1,24 +1,18 @@
 from typing import Any
 
-from Cython.Build import cythonize
-from Cython.Distutils.build_ext import new_build_ext
 from setuptools import Extension
 
 
 def build(setup_kwargs: dict[str, Any]) -> None:
     setup_kwargs |= {
-        "ext_modules": cythonize(
-            module_list=[
-                Extension(
-                    name="audiometer",
-                    sources=["src/audiometer/main.pyx"],
-                    libraries=["m"],
-                )
-            ],
-            compiler_directives={
-                "language_level": "3",
-                "always_allow_keywords": True,
-            },
-        ),
-        "cmdclass": {"build_ext": new_build_ext},
+        "ext_modules": [
+            Extension(
+                name="audiometer",
+                sources=["src/audiometer/main.c"],
+                libraries=["m"],
+            )
+        ],
+        "package_data": {
+            "audiometer": ["*.pyi", "py.typed"],
+        },
     }
