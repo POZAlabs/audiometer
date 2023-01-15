19406375fa624dad6cdc894bfd6e3977bcbd7a0e
novdov <rpwjd56@gmail.com>
2023-01-15 22:08:14 +0900

    c: python 래퍼 구현

--- python/audiometer/__init__.py
+++ python/audiometer/__init__.py
@@ -1,3 +1,3 @@
-from ._audiometer import calculate_rms
+from ._audiometer import calculate_peak, calculate_rms
 
-__all__ = ["calculate_rms"]
+__all__ = ["calculate_rms", "calculate_peak"]
--- python/audiometer/_audiometer.py
+++ python/audiometer/_audiometer.py
@@ -1,6 +1,6 @@
 import pydub
 
-from .audiometer import calculate_rms_inner
+from .audiometer import calculate_peak_inner, calculate_rms_inner
 
 
 def calculate_rms(segment: pydub.AudioSegment) -> float:
@@ -10,6 +10,17 @@ def calculate_rms(segment: pydub.AudioSegment) -> float:
             channels=segment.channels,
             max_amplitude=segment.max_possible_amplitude,
             sample_rate=segment.frame_rate,
+        ),
+        1,
+    )
+
+
+def calculate_peak(segment: pydub.AudioSegment) -> float:
+    return round(
+        calculate_peak_inner(
+            samples=segment.get_array_of_samples(),
+            channels=segment.channels,
+            max_amplitude=segment.max_possible_amplitude,
         ),
         1,
     )
