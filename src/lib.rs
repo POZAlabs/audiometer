4acc7623e9486f7082cbc34c62bfe2e370e94a8d
novdov <rpwjd56@gmail.com>
2023-01-15 22:05:51 +0900

    c: peak 계산 Rust로 포팅

--- src/lib.rs
+++ src/lib.rs
@@ -39,9 +39,26 @@ fn calculate_rms_inner(
     ratio_to_db(max_rms * AMPLITUDE_COEFFICIENT, false)
 }
 
+#[pyfunction]
+fn calculate_peak_inner(samples: Vec<isize>, channels: usize, max_amplitude: f64) -> f64 {
+    let mut max_peak: f64 = 0.0;
+    for i in 0..channels {
+        let mut channel_max_peak: f64 = 0.0;
+        for channel_sample in samples[i..].iter().step_by(channels) {
+            let sample = (*channel_sample as f64 / max_amplitude).abs();
+            channel_max_peak = channel_max_peak.max(sample)
+        }
+
+        max_peak = max_peak.max(channel_max_peak)
+    }
+
+    ratio_to_db(max_peak, true)
+}
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn audiometer(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(calculate_rms_inner, m)?)?;
+    m.add_function(wrap_pyfunction!(calculate_peak_inner, m)?)?;
     Ok(())
 }
