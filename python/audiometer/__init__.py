38ce1e6bee11e6b5c75f21b6cb478274e0ffe895
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2024-08-22 15:57:02 +0900

    d: pydub 의존성 제거

--- python/audiometer/__init__.py
+++ python/audiometer/__init__.py
@@ -1,17 +1,7 @@
-from ._audiometer import convert_24bit_to_32bit
-from ._wrapper import (
-    calculate_integrated_loudness,
-    calculate_lufs,
-    calculate_momentary_loudness,
-    calculate_peak,
-    calculate_rms,
-)
+from ._audiometer import convert_24bit_to_32bit, measure_peak, measure_rms
 
 __all__ = [
-    "calculate_rms",
-    "calculate_peak",
-    "calculate_integrated_loudness",
-    "calculate_momentary_loudness",
-    "calculate_lufs",
     "convert_24bit_to_32bit",
+    "measure_peak",
+    "measure_rms",
 ]
