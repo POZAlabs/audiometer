7d3b17408a1b59ec4814fd5199ccb77dca481214
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2025-01-06 17:30:04 +0900

    IP-6106: [audiometer] peak, rms 계산 최적화 (#26)

--- src/peak.rs
+++ src/peak.rs
@@ -1,13 +1,14 @@
 use pyo3::pyfunction;
 
+use crate::types;
 use crate::utils::ratio_to_db;
 
 #[pyfunction]
-pub fn measure_peak(samples: Vec<isize>, channels: usize, max_amplitude: f64) -> f64 {
+pub fn measure_peak(samples: types::Samples, channels: usize, max_amplitude: f64) -> f64 {
     let mut max_peak: f64 = 0.0;
     for i in 0..channels {
         let mut channel_max_peak: f64 = 0.0;
-        for channel_sample in samples[i..].iter().step_by(channels) {
+        for channel_sample in samples.source[i..].iter().step_by(channels) {
             let sample = (*channel_sample as f64 / max_amplitude).abs();
             channel_max_peak = channel_max_peak.max(sample)
         }
